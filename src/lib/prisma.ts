fc90a6b4397d487225d287685eb131a16261bb9f
Emilio F <emiliofurrer00@gmail.com>
2025-12-21 03:54:00 +0100

    add logs to debug

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
 
+console.log("Has DATABASE_URL key:", Object.prototype.hasOwnProperty.call(process.env, "DATABASE_URL"));
+console.log("DATABASE_URL length:", process.env.DATABASE_URL?.length ?? 0);
 const url = process.env.DATABASE_URL;
 if (!url) throw new Error("DATABASE_URL is not set");
 
