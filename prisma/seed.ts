df370d57051e1dc7a01a81fa3f30bd01521a9ad8
Emilio F <emiliofurrer00@gmail.com>
2025-12-27 23:15:25 +0100

    add docker-compose to spin up local db postgres instance

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, QuestionType } from "../src/generated/prisma/client";
+import { PrismaClient, QuestionType } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
